75495f82a6f5f193005291fbfa56317b455ac1c9
andre <andreavendetti02@gmail.com>
2026-03-13 13:13:10 +0100

    primo commit: struttura progetto java

---
+++ src/test/java/it/home/PaoloFrattarolaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaoloFrattarolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
