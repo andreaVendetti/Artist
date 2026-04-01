0b5392602021f83bd1bae546a36e632a7ee6fc50
Andrea Vendetti <andreavendetti02@gmail.com>
2026-04-01 12:21:31 +0200

    da aggiungere funzionalità aggiuntive (da richiedere)

--- src/main/java/it/home/config/CorsConfig.java
+++ src/main/java/it/home/config/CorsConfig.java
@@ -4,21 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class CorsConfig {
-
     @Bean
     WebMvcConfigurer corsConfigurer() {
-	        return new WebMvcConfigurer() {
-	            @Override
-	            public void addCorsMappings(CorsRegistry registry) {
-	                registry.addMapping("/**")
-	                        .allowedOrigins("http://localhost:4200")
-	                        .allowedMethods("*")
-	                        .allowedHeaders("*")
-	                        .allowCredentials(true);
-	            }
-	        };
-	    }
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:4200",
+                            "http://172.20.10.3:4200",  // IP hotspot
+                            "http://192.168.1.*"         // rete locale
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
